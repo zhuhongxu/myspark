3c8d49b7cb052cdd455b77ad4f9c5ab97d3d8a82
zhuhongxu <zhuhx@degnity.com>
2019-01-22 10:35:00 +0800

    list rdd实例

--- src/main/java/com/zhx/rdd/ListDemo.scala
+++ src/main/java/com/zhx/rdd/ListDemo.scala
@@ -14,9 +14,92 @@ object ListDemo {
     val conf = new SparkConf().setMaster("local").setAppName("myspark")
     val sc = new SparkContext(conf)
 
-    testWordCount(sc)
+//    testWordCount(sc)
+//    testFilter(sc)
+//    testUnion(sc)
+//    testCount(sc)
+//    testTake(sc)
+//    testMap(sc)
+//    testFlatMap(sc)
+//    testReduce(sc)
+      testAggregate(sc)
+
+  }
+
+  def testAggregate(sc: SparkContext): Unit ={
+    val input = sc.parallelize(List(1, 2, 3, 4, 5, 6, 7, 8), 3)
+    println(input.aggregate(1)(_+_, _+_))
+  }
 
 
+
+  /**
+    * 测试reduce
+    * @param sc
+    */
+  def testReduce(sc: SparkContext): Unit ={
+    println(sc.parallelize(1 to 10).reduce(_+_))
+
+  }
+
+  /**
+    * 测试flatMap
+    * @param sc
+    */
+  def testFlatMap(sc: SparkContext): Unit ={
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
+    lines.flatMap(_.split(" ").toVector).foreach(println)
+  }
+
+  /**
+    * 测试map
+    * @param sc
+    */
+  def testMap(sc: SparkContext): Unit ={
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
+    lines.map(_.split(" ").toVector).foreach(println)
+  }
+
+  /**
+    * 测试take
+    * @param sc
+    */
+  def testTake(sc: SparkContext): Unit ={
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
+    val error = lines.filter(line => line.contains("error"))
+    error.take(1).foreach(println)
+  }
+
+  /**
+    * 测试记数
+    * @param sc
+    */
+  def testCount(sc: SparkContext): Unit ={
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
+    val error = lines.filter(line => line.contains("error"))
+    println(error.count())
+  }
+
+  /**
+    * 测试合并
+    * @param sc
+    */
+  def testUnion(sc: SparkContext): Unit ={
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
+    val error = lines.filter(line => line.contains("error"))
+    val warn = lines.filter(line => line.contains("warn"))
+    error.union(warn).foreach(println)
+  }
+
+  /**
+    * 测试过滤器
+    * @param sc
+    */
+  def testFilter(sc: SparkContext): Unit ={
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
+
+    //打印出文本文件中所有包含error的行
+    lines.filter(_.contains("error")).foreach(println)
   }
 
   /**
@@ -24,7 +107,7 @@ object ListDemo {
     * @param sc
     */
   def testWordCount(sc: SparkContext): Unit ={
-    val lines = sc.textFile("src/main/java/com/zhx/rdd/wordCountData")
+    val lines = sc.textFile("src/main/java/com/zhx/rdd/testTxt")
     lines.flatMap(_.split(" ")).map((_ , 1)).reduceByKey(_ + _).foreach(println)
     sc.stop()
   }
